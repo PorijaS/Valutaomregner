33039f83095d89e4ba05150b4faeb3dde318804a
Porija Najafi Nejadian Shiraz <pori0001@edu.sde.dk>
2020-11-02 10:51:23 +0100

    Add project files.

---
+++ Valutaomregner/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Valutaomregner
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.amount_text = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.display_txt = new System.Windows.Forms.Label();
+			this.conv_btn = new System.Windows.Forms.Button();
+			this.clear_btn = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(141, 138);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(144, 21);
+			this.comboBox1.TabIndex = 0;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(141, 198);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(144, 21);
+			this.comboBox2.TabIndex = 0;
+			this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+			// 
+			// amount_text
+			// 
+			this.amount_text.Location = new System.Drawing.Point(141, 79);
+			this.amount_text.Name = "amount_text";
+			this.amount_text.Size = new System.Drawing.Size(144, 20);
+			this.amount_text.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(54, 79);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(71, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Enter Amount";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(54, 141);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(70, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Convert From";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(54, 201);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(60, 13);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Convert To";
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// display_txt
+			// 
+			this.display_txt.AutoSize = true;
+			this.display_txt.Location = new System.Drawing.Point(169, 263);
+			this.display_txt.Name = "display_txt";
+			this.display_txt.Size = new System.Drawing.Size(0, 13);
+			this.display_txt.TabIndex = 5;
+			// 
+			// conv_btn
+			// 
+			this.conv_btn.Location = new System.Drawing.Point(141, 288);
+			this.conv_btn.Name = "conv_btn";
+			this.conv_btn.Size = new System.Drawing.Size(144, 22);
+			this.conv_btn.TabIndex = 6;
+			this.conv_btn.Text = "Convert";
+			this.conv_btn.UseVisualStyleBackColor = true;
+			this.conv_btn.Click += new System.EventHandler(this.conv_btn_Click);
+			// 
+			// clear_btn
+			// 
+			this.clear_btn.Location = new System.Drawing.Point(141, 317);
+			this.clear_btn.Name = "clear_btn";
+			this.clear_btn.Size = new System.Drawing.Size(144, 23);
+			this.clear_btn.TabIndex = 7;
+			this.clear_btn.Text = "Clear";
+			this.clear_btn.UseVisualStyleBackColor = true;
+			this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+			this.ClientSize = new System.Drawing.Size(426, 450);
+			this.Controls.Add(this.clear_btn);
+			this.Controls.Add(this.conv_btn);
+			this.Controls.Add(this.display_txt);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.amount_text);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.comboBox1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.TextBox amount_text;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label display_txt;
+		private System.Windows.Forms.Button conv_btn;
+		private System.Windows.Forms.Button clear_btn;
+	}
+}
+
---
+++ Valutaomregner/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Valutaomregner
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+		
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			using var reader = XmlReader.Create("https://www.nationalbanken.dk/_vti_bin/DN/DataService.svc/CurrencyRatesXML?lang=en");
+			
+			var xs = new XmlSerializer(typeof(Valutakurser));
+			Valutakurser VK = (Valutakurser)xs.Deserialize(reader);
+
+			DataTable DT = new DataTable();
+			DT.Columns.Add("text");
+			DT.Columns.Add("value");
+			DT.Rows.Add("---SELECT---", 0);
+
+			DataTable DT2 = new DataTable();
+			DT2.Columns.Add("text2");
+			DT2.Columns.Add("value2");
+			DT2.Rows.Add("---SELECT---", 0);
+
+			foreach (Currency CS in VK.dailyRates.Currencies)
+			{
+				DT.Rows.Add(CS.Code, CS.Rate);
+				DT2.Rows.Add(CS.Code, CS.Rate);
+			}
+
+			comboBox1.DataSource = DT.DefaultView;
+			comboBox1.DisplayMember = "text";
+			comboBox1.ValueMember = "value";
+			comboBox1.SelectedIndex = 0;
+
+
+			comboBox2.DataSource = DT2.DefaultView;
+			comboBox2.DisplayMember = "text2";
+			comboBox2.ValueMember = "value2";
+			comboBox2.SelectedIndex = 0;
+		}
+
+		[XmlRoot(ElementName = "exchangerates")]
+		public class Valutakurser
+		{
+			[XmlAttribute]
+			public DateTime id { get; set; }
+
+			[XmlElement("dailyrates")]
+			public DailyRates dailyRates { get; set; }
+		}
+
+		public class DailyRates
+		{
+			[XmlElement("currency")]
+			public List<Currency> Currencies { get; set; }
+		}
+
+		public class Currency
+		{
+			[XmlAttribute("code")]
+			public string Code { get; set; }
+
+			[XmlAttribute("desc")]
+			public string Desc { get; set; }
+
+			[XmlAttribute("rate")]
+			public double Rate { get; set; }
+
+			public Currency() { }
+
+		}
+
+		private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void conv_btn_Click(object sender, EventArgs e)
+		{
+			double ConvertedValue;
+
+			if (amount_text.Text == null || amount_text.Text.Trim() == "")
+			{
+				MessageBox.Show("Please Enter Currency", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				amount_text.Focus();
+				return;
+			}
+			else if (comboBox1.SelectedValue == null || comboBox1.SelectedIndex == 0)
+			{
+				MessageBox.Show("Please Select Currency From", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				comboBox1.Focus();
+				return;
+			}
+			else if (comboBox2.SelectedValue == null || comboBox2.SelectedIndex == 0)
+			{
+				MessageBox.Show("Please Select Currency To", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				comboBox2.Focus();
+				return;
+			}
+
+			if (comboBox1.Text == comboBox2.Text)
+			{
+				ConvertedValue = double.Parse(amount_text.Text);
+				display_txt.Text = comboBox2.Text + " " + ConvertedValue.ToString("N3");
+			}
+			else
+			{
+				ConvertedValue = (double.Parse(comboBox1.SelectedValue.ToString()) * double.Parse(amount_text.Text)) / double.Parse(comboBox2.SelectedValue.ToString());
+				display_txt.Text = comboBox2.Text + " " + ConvertedValue.ToString("N3");
+			}
+		}
+
+		private void clear_btn_Click(object sender, EventArgs e)
+		{
+			ClearControls();
+		}
+
+		private void ClearControls()
+		{
+			amount_text.Text = string.Empty;
+			if (comboBox1.Items.Count > 0)
+				comboBox1.SelectedIndex = 0;
+			if (comboBox2.Items.Count > 0)
+				comboBox2.SelectedIndex = 0;
+			display_txt.Text = "";
+			amount_text.Focus();
+		}
+
+		private void display_txt_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}	
